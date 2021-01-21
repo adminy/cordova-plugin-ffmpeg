02950241aa0a6e59347539178d0f614af33b53cd
Zak Barbuto <zbarbuto@nextfaze.com>
2021-01-21 11:59:25 +1030

    Use asynchronous execution

--- src/android/FFMpeg.java
+++ src/android/FFMpeg.java
@@ -3,6 +3,7 @@ package com.marin.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import com.arthenica.mobileffmpeg.ExecuteCallback;
 import com.arthenica.mobileffmpeg.FFmpeg;
 import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
  // ref: https://github.com/tanersener/mobile-ffmpeg/wiki/Android
@@ -11,11 +12,15 @@ public class FFMpeg extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("exec")) {
-            int returnCode = FFmpeg.execute(data.getString(0));
-            if (returnCode == RETURN_CODE_SUCCESS)
-                callbackContext.success("Done");
-            else
-                callbackContext.error("Error Code: " + returnCode);
+            FFmpeg.executeAsync(data.getString(0), new ExecuteCallback() {
+                @Override
+                public void apply(long executionId, int returnCode) {
+                    if (returnCode == RETURN_CODE_SUCCESS)
+                        callbackContext.success("Done");
+                    else
+                        callbackContext.error("Error Code: " + returnCode);
+                }
+            });
             return true;
         } else return false;
     }
